72d6272d45962c3ad5cb0bb340db5b51a546ab57
Anton <a.chertok@akatech.com.ua>
2022-11-09 11:59:12 +0200

    Update Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,45 +1,39 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 const modalRoot = document.querySelector('#modal-root');
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.onEscClose);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.onEscClose);
-  }
-
-  onEscClose = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
-
-  onBackdropClick = e => {
+const Modal = ({ image: { src, alt }, onClose }) => {
+  const onBackdropClick = e => {
     if (e.target === e.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  render() {
-    const {
-      image: { src, alt },
-    } = this.props;
+  useEffect(() => {
+    const onEscClose = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
+    };
 
-    return createPortal(
-      <div className="Overlay" onClick={this.onBackdropClick}>
-        <div className="Modal">
-          <img src={src} alt={alt} />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+    window.addEventListener('keydown', onEscClose);
+
+    return () => {
+      window.removeEventListener('keydown', onEscClose);
+    };
+  }, [onClose]);
+
+  return createPortal(
+    <div className="Overlay" onClick={onBackdropClick}>
+      <div className="Modal">
+        <img src={src} alt={alt} />
+      </div>
+    </div>,
+    modalRoot
+  );
+};
 
 Modal.propTypes = {
   src: PropTypes.string,
